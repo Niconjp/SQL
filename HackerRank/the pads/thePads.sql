d1a8850883bce99855ae811170457d78d02e4590
Niconjp <102393661+Niconjp@users.noreply.github.com>
2022-05-10 15:42:57 -0300

    Create thePads.sql

---
+++ HackerRank/the pads/thePads.sql
@@ -0,0 +1,4 @@
+/* https://www.hackerrank.com/challenges/the-pads/problem */
+
+SELECT CONCAT(Name,"(",left(Occupation,1),")") FROM OCCUPATIONS ORDER BY Name;
+SELECT "There are a total of ",COUNT(Occupation) AS Oc,LOWER(CONCAT(Occupation,"s.")) FROM OCCUPATIONS GROUP BY Occupation ORDER BY Oc,Occupation;
