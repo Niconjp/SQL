a0072bf229208ca4f37cf46c083082878646f1f7
Niconjp <102393661+Niconjp@users.noreply.github.com>
2022-05-10 17:32:40 -0300

    Create weather-observation-station-19.sql

---
+++ HackerRank/weather-observation-station-19.sql
@@ -0,0 +1,6 @@
+/* https://www.hackerrank.com/challenges/weather-observation-station-19/problem */
+
+SELECT TRUNCATE( SQRT(POWER((MAX(LAT_N) - MIN(LAT_N)),2) + 
+                 POWER((MAX(LONG_W) - MIN(LONG_W)),2)
+                ), 4) 
+FROM STATION;
