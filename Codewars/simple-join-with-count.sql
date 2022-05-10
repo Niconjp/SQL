5015f9c3ef1ccde08e93635cbdfa3fbe5e8a2710
Niconjp <102393661+Niconjp@users.noreply.github.com>
2022-05-10 18:23:13 -0300

    7 kyu

---
+++ Codewars/simple-join-with-count.sql
@@ -0,0 +1,11 @@
+/* https://www.codewars.com/kata/580918e24a85b05ad000010c/sql */
+
+/* SQL Basics: Simple JOIN with COUNT */
+
+/* 7 kyu */
+
+SELECT p1.id,p1.name,count(t2.id) AS toy_count 
+FROM people p1 
+INNER JOIN toys t2
+ON t2.people_id = p1.id
+GROUP BY p1.id;
