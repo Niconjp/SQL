90f911456f02372f5d0b93a5b6947aae125d4347
Niconjp <102393661+Niconjp@users.noreply.github.com>
2022-05-24 19:56:28 -0300

    8 kyu

---
+++ Codewars/rounding-decimals.sql
@@ -0,0 +1,11 @@
+ /* https://www.codewars.com/kata/594a6133704e4daf5d00003d/sql */
+ 
+/* Easy SQL: Rounding Decimals */
+
+/* 8 kyu */
+
+SELECT
+  FLOOR(number1) AS number1,
+  CEILING(number2) AS number2
+FROM
+  decimals
