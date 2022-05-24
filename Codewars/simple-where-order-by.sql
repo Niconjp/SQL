6a17cd757417e2214beedf4d754bb7c81353e6a4
Niconjp <102393661+Niconjp@users.noreply.github.com>
2022-05-24 19:54:04 -0300

    8 kyu

---
+++ Codewars/simple-where-order-by.sql
@@ -0,0 +1,14 @@
+/* https://www.codewars.com/kata/5809508cc47d327c12000084/sql */
+
+/* 8 kyu */
+
+/* SQL Basics: Simple WHERE and ORDER BY */
+
+SELECT 
+  * 
+FROM 
+  people
+WHERE 
+  age > 50
+ORDER BY 
+  age DESC
