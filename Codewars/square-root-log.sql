fbe52021c1ac5a24221c80923b3e15e2e71d43f7
Niconjp <102393661+Niconjp@users.noreply.github.com>
2022-05-24 19:57:14 -0300

    8 kyu

---
+++ Codewars/square-root-log.sql
@@ -0,0 +1,11 @@
+/* https://www.codewars.com/kata/594a691720ac16a544000075/sql */
+
+/* Easy SQL: Square Root and Log */
+
+/* 8 kyu */
+
+SELECT
+  SQRT(number1) AS root,
+  LOG(number2) AS log
+FROM
+  decimals
