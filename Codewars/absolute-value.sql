aac9354ac3d465061dc83cae3ff72312c991b33e
Niconjp <102393661+Niconjp@users.noreply.github.com>
2022-05-24 20:00:18 -0300

    7 kyu

---
+++ Codewars/absolute-value.sql
@@ -0,0 +1,11 @@
+/* https://www.codewars.com/kata/594a8f2f7ca3c692a4000041/sql */
+
+/* Easy SQL: Absolute Value and Log to Base */
+
+/* 7 kyu */
+
+SELECT
+  ABS(number1) AS abs,
+  LOG(64,number2) AS log
+FROM
+  decimals
